245dc6d63be361c702999404cc87ea277f141667
Jeremy Fee <jmfee@usgs.gov>
2015-04-06 10:58:37 -0600

    Add comments, stop parsing already-json response in cache invalidator

--- src/htdocs/js/CacheInvalidator.js
+++ src/htdocs/js/CacheInvalidator.js
@@ -9,6 +9,14 @@ var DEFAULTS = {
 };
 
 
+/**
+ * Cache invalidator utility.
+ *
+ * @param options {Object}
+ * @param options.url {String}
+ *        url for cache invalidation requests.
+ *        should accept 'path' parameter via POST.
+ */
 var CacheInvalidator = function (options) {
   var _this,
       _initialize,
@@ -23,14 +31,26 @@ var CacheInvalidator = function (options) {
     options = null;
   };
 
+  /**
+   * Invalidate a path.
+   *
+   * @param path {String}
+   *        site-relative path.
+   * @param callback {Function(status, xhr, data)}
+   *        called after invalidation request is complete.
+   *        status {Number} http response status
+   *        xhr {XMLHttpRequest} request object.
+   *        data {Object} status codes for different servers.
+   */
   _this.invalidate = function (path, callback) {
     Xhr.ajax({
+      url: _url,
       method: 'POST',
       data: {
         path: path
       },
       success: function (response, xhr) {
-        callback(xhr.status, xhr, JSON.parse(response));
+        callback(xhr.status, xhr, response);
       },
       error: function (status, xhr) {
         callback(xhr.status, xhr, null);
